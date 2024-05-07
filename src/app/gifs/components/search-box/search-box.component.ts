fbcc79819210a20e65cd7624aabae624f1c5fd1b
Patrick Suclla <sucllaerazop@gmail.com>
2024-05-07 13:05:30 -0500

    creacion de app y diseño de interface

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTageInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private GifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.GifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
